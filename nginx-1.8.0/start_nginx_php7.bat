28f7e02e2e5c191416b646c579c17a7148008595
[xiaowenjie] <[xiaowenjie@haoche51.com]>
2016-05-27 10:19:21 +0800

    a

---
+++ nginx-1.8.0/start_nginx_php7.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Windows 下无效
+REM set PHP_FCGI_CHILDREN=5
+
+REM 每个进程处理的最大请求数，或设置为 Windows 环境变量
+set PHP_FCGI_MAX_REQUESTS=1000
+ 
+echo Starting PHP FastCGI...
+RunHiddenConsole C:/php-7.0.0-nts-Win32-VC14-x64/php-cgi.exe -b 127.0.0.1:9000
+
+echo Starting nginx...
+RunHiddenConsole C:/nginx-1.8.0/nginx.exe
